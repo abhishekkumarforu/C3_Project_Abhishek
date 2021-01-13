7d3bb8c7798f87759032c9f794c8f1c0687094a4
abhishekkumarforu <abhishekkumarforu@gmail.com>
2021-01-13 19:23:29 +0530

    Initial commit

---
+++ src/main/java/RestaurantService.java
@@ -0,0 +1,42 @@
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RestaurantService {
+    private static List<Restaurant> restaurants = new ArrayList<>();
+
+    public static Restaurant findRestaurantByName(String restaurantName){
+
+        Restaurant matchRestaurant = null;
+
+        for (Restaurant restaurant : restaurants){
+
+            String name = restaurant.getName();
+            if (name.contains(restaurantName)){
+                matchRestaurant = restaurant ;
+                break ;
+            }
+        }
+
+        return matchRestaurant ;
+
+
+    }
+
+
+    public Restaurant addRestaurant(String name, String location, LocalTime openingTime, LocalTime closingTime) {
+        Restaurant newRestaurant = new Restaurant(name, location, openingTime, closingTime);
+        restaurants.add(newRestaurant);
+        return newRestaurant;
+    }
+
+    public Restaurant removeRestaurant(String restaurantName) throws restaurantNotFoundException {
+        Restaurant restaurantToBeRemoved = findRestaurantByName(restaurantName);
+        restaurants.remove(restaurantToBeRemoved);
+        return restaurantToBeRemoved;
+    }
+
+    public List<Restaurant> getRestaurants() {
+        return restaurants;
+    }
+}
